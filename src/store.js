e2ca848b9f6c06a09dd1681d161b32fed8db45af
nyxynyx <nyxynyx@gmail.com>
2020-04-29 10:25:53 -0400

    Importing `redux-beacon-electon` causes error: Uncaught TypeError: fs.existsSync is not a function

--- src/store.js
+++ src/store.js
@@ -3,6 +3,16 @@ import { connectRouter, routerMiddleware, push } from 'connected-react-router';
 import persistState from 'redux-localstorage';
 import thunk from 'redux-thunk';
 
+// importing `redux-beacon-electron` modules below will cause this error in Electron JS console:
+//      Uncaught TypeError: fs.existsSync is not a function
+import { 
+    createElectronGoogleAnalyticsTarget, 
+    actionMetaEventMapper as eventsMapper
+} from 'redux-beacon-electron'
+
+const electronTarget = createElectronGoogleAnalyticsTarget({ua: 'UA-XXXX'})
+const gaMiddleware = createMiddleware(eventsMapper, electronTarget)
+
 export default function configureStore(initialState, routerHistory) {
   const router = routerMiddleware(routerHistory);
 
@@ -14,7 +24,7 @@ export default function configureStore(initialState, routerHistory) {
     router: connectRouter(routerHistory),
   };
 
-  const middlewares = [thunk, router];
+  const middlewares = [thunk, router, gaMiddleware];
 
   const composeEnhancers = (() => {
     const compose_ = window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
